0865a9d5710f1bd438291524c56c73aac3e63f5a
Risvan Muhammad <risvan145k@gmail.com>
2026-06-07 13:03:09 +0530

    Extend icon support for video and audio files

--- icons.py
+++ icons.py
@@ -30,9 +30,23 @@ IMAGE_ICON = Icon(
     Color(222, 0, 255, 255),
 )
 
+VIDEO_ICON = Icon(
+    "",
+    Color(222, 0, 255, 255),
+)
+
+AUDIO_ICON = Icon(
+    "",
+    Color(222, 0, 255, 255),
+)
+
 icon_map: dict[str, Icon] = {
     ".py": Icon("", Color(69, 239, 255, 255)),
     ".c": Icon("", Color(0, 145, 255, 255)),
+    ".txt": Icon("", Color(222, 222, 222, 255)),
+    ".sh": Icon("", Color(68, 255, 0, 255)),
+    ".bash": Icon("", Color(68, 255, 0, 255)),
+    ".json": Icon("", Color(245, 255, 56, 255)),
     ".toml": Icon("", Color(0, 200, 255, 255)),
     ".md": Icon("", Color(0, 123, 255, 255)),
     ".lock": Icon("󰌾", Color(255, 145, 0, 255)),
@@ -40,6 +54,10 @@ icon_map: dict[str, Icon] = {
     ".png": IMAGE_ICON,
     ".jpg": IMAGE_ICON,
     ".jpeg": IMAGE_ICON,
+    ".mp3": AUDIO_ICON,
+    ".wav": AUDIO_ICON,
+    ".mp4": VIDEO_ICON,
+    ".mkv": VIDEO_ICON,
 }
 
 ICONS = set([DIR_ICON, FILE_ICON, LICENSE_ICON, MAKE_FILE_ICON]) | set(
