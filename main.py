d295aeb0c60def39d5e9dba013d6770b3c2da5c7
Risvan Muhammad <risvan145k@gmail.com>
2026-06-09 22:03:21 +0530

    Implement grid system

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from enum import Enum, auto
 from pathlib import Path
 from typing import Self
 
-from pyray import Color, ConfigFlags, Font, KeyboardKey as Key, TextureFilter
+from pyray import (
+    Color,
+    ConfigFlags,
+    Font,
+    KeyboardKey as Key,
+    TextureFilter,
+)
 import pyray as ray
 
 from icons import (
+    Icon,
     ICONS,
     DIR_ICON,
     FILE_ICON,
-    ICONS,
     LICENSE_ICON,
     MAKE_FILE_ICON,
-    Icon,
     icon_map,
 )
 
-
-FONT_SIZE = 24.0
-SPACING = 1.0
 
 FG_COLOR = Color(255, 255, 255, 255)
 BG_COLOR = Color(30, 30, 30, 255)
@@ -29,9 +31,6 @@ SELECTION_COLOR = Color(52, 52, 52, 100)
 
 DIR_COLOR = Color(66, 135, 245, 255)
 FILE_COLOR = Color(255, 255, 255, 255)
-
-font_regular: Font | None = None
-font_bold: Font | None = None
 
 
 class EntryType(Enum):
@@ -67,7 +66,52 @@ class State:
     cwd: Path
     files: list[Entry]
     selected_idx: int
+    grid: Grid
     scroll_top: int = 0
+
+
+@dataclass(frozen=True)
+class Grid:
+    cell_width: int
+    cell_height: int
+    font_size: float
+    font_regular: Font
+    font_bold: Font
+
+    def rows(self) -> int:
+        return ray.get_screen_height() // self.cell_height
+
+    def cell_to_pixel(self, col: int, row: int) -> tuple[int, int]:
+        return col * self.cell_width, row * self.cell_height
+
+    def draw_line(self, row: int, fill: Color) -> None:
+        w = ray.get_screen_width() // self.cell_width
+        self.draw_rect(0, row, w, 1, fill)
+
+    def draw_rect(
+        self, col: int, row: int, width: int, height: int, fill: Color
+    ) -> None:
+        x, y = self.cell_to_pixel(col, row)
+        w, h = self.cell_to_pixel(width, height)
+        ray.draw_rectangle(x, y, w, h, fill)
+
+    def draw_text(
+        self,
+        text: str,
+        col: int,
+        row: int,
+        color: Color = FG_COLOR,
+        bold: bool = False,
+    ) -> None:
+        font = self.font_bold if bold else self.font_regular
+        ray.draw_text_ex(
+            font,
+            text,
+            self.cell_to_pixel(col, row),
+            self.font_size,
+            1.0,
+            color,
+        )
 
 
 def get_icon(path: Path) -> Icon:
@@ -84,31 +128,9 @@ def get_icon(path: Path) -> Icon:
     return icon_map.get(path.suffix, FILE_ICON)
 
 
-def draw_text(
-    text: str, x: int, y: int, color: Color = FG_COLOR, bold: bool = False
-) -> None:
-    assert font_regular and font_bold
-
-    font = font_bold if bold else font_regular
-    ray.draw_text_ex(
-        font, text, (float(x), float(y)), FONT_SIZE, SPACING, color
-    )
-
-
-def get_line_size() -> tuple[int, int]:
-    assert font_regular
-    text_size = ray.measure_text_ex(font_regular, "M", FONT_SIZE, SPACING)
+def get_char_size(font: Font, font_size: float) -> tuple[int, int]:
+    text_size = ray.measure_text_ex(font, "M", font_size, 1.0)
     return int(text_size.x), int(text_size.y)
-
-
-def draw_line(line: int, color: Color) -> None:
-    assert font_regular
-    width = ray.get_screen_width()
-    _, height = get_line_size()
-
-    x = 0
-    y = line * height
-    ray.draw_rectangle(x, y, width, height, color)
 
 
 def list_dir(path: Path) -> list[Entry]:
@@ -124,9 +146,7 @@ def list_dir(path: Path) -> list[Entry]:
     )
 
 
-def load_fonts() -> None:
-    global font_regular, font_bold
-
+def load_fonts(font_size: int) -> tuple[Font, Font]:
     # Standard 95 printable ASCII characters (codes 32 to 126)
     codepoints = tuple(range(32, 127))
     codepoints = codepoints + tuple(ord(icon.char) for icon in ICONS)
@@ -137,7 +157,7 @@ def load_fonts() -> None:
 
     font_regular = ray.load_font_ex(
         "./fonts/JetBrainsMonoNerdFontMono-Regular.ttf",
-        int(FONT_SIZE),
+        font_size,
         c_ptr,
         count,
     )
@@ -147,7 +167,7 @@ def load_fonts() -> None:
 
     font_bold = ray.load_font_ex(
         "./fonts/JetBrainsMonoNerdFontMono-Bold.ttf",
-        int(FONT_SIZE),
+        font_size,
         c_ptr,
         count,
     )
@@ -155,13 +175,12 @@ def load_fonts() -> None:
         font_bold.texture, TextureFilter.TEXTURE_FILTER_BILINEAR
     )
 
+    return font_regular, font_bold
 
-def unload_fonts() -> None:
-    if font_regular:
-        ray.unload_font(font_regular)
 
-    if font_bold:
-        ray.unload_font(font_bold)
+def unload_fonts(*fonts: Font) -> None:
+    for font in fonts:
+        ray.unload_font(font)
 
 
 def is_key_pressed(key: int, repeat: bool = True) -> bool:
@@ -171,8 +190,7 @@ def is_key_pressed(key: int, repeat: bool = True) -> bool:
 
 
 def handle_input(state: State) -> None:
-    _, line_height = get_line_size()
-    visible_rows = (ray.get_screen_height() // line_height) - 1  # For top bar
+    visible_rows = state.grid.rows() - 1  # For top bar
 
     if is_key_pressed(Key.KEY_J):
         state.selected_idx = min(state.selected_idx + 1, len(state.files) - 1)
@@ -184,7 +202,9 @@ def handle_input(state: State) -> None:
         if state.selected_idx < state.scroll_top:
             state.scroll_top = state.selected_idx
 
-    elif is_key_pressed(Key.KEY_ENTER, False) and state.files:
+    elif (
+        is_key_pressed(Key.KEY_ENTER, False) or is_key_pressed(Key.KEY_L, False)
+    ) and state.files:
         file = state.files[state.selected_idx]
         if file.etype == EntryType.DIR:
             state.cwd = file.path
@@ -204,28 +224,29 @@ def main():
     files = list_dir(cwd)
     selected_idx = 0
 
-    state = State(cwd, files, selected_idx)
-
     ray.set_config_flags(ConfigFlags.FLAG_WINDOW_RESIZABLE)
 
     ray.init_window(800, 800, "Raccoon")
     ray.set_target_fps(60)
     ray.set_exit_key(Key.KEY_Q)
-    load_fonts()
 
-    pad_x = 18
-    line_width, line_height = get_line_size()
+    font_size = 24
+    font_regular, font_bold = load_fonts(font_size)
+
+    pad_col = 2
+    char_width, char_height = get_char_size(font_regular, float(font_size))
+    grid = Grid(char_width, char_height, font_size, font_regular, font_bold)
+    state = State(cwd, files, selected_idx, grid)
     while not ray.window_should_close():
         handle_input(state)
 
         ray.begin_drawing()
         ray.clear_background(BG_COLOR)
 
-        draw_line(0, Color(20, 20, 20, 150))
-        draw_text(state.cwd.as_posix(), pad_x, 0)
+        grid.draw_line(0, Color(20, 20, 20, 150))
+        grid.draw_text(state.cwd.as_posix(), pad_col, 0)
 
-        start_row = 1
-        visible_rows = (ray.get_screen_height() // line_height) - 1
+        visible_rows = (ray.get_screen_height() // char_height) - 1
         visible_files = state.files[
             state.scroll_top : state.scroll_top + visible_rows
         ]
@@ -237,21 +258,21 @@ def main():
             bold = False
             if real_idx == state.selected_idx:
                 bold = True
-                draw_line(line, SELECTION_COLOR)
+                grid.draw_line(line, SELECTION_COLOR)
 
-            x = pad_x
-            y = line_height * line
+            col = pad_col
+            row = line
             icon = file.icon
-            draw_text(icon.char, x, y, color=icon.color, bold=bold)
+            grid.draw_text(icon.char, col, row, color=icon.color, bold=bold)
 
-            x += 2 * line_width
-            draw_text(file.name, x, y, color=file.color, bold=bold)
+            col += 2
+            grid.draw_text(file.name, col, row, color=file.color, bold=bold)
 
             line += 1
 
         ray.end_drawing()
 
-    unload_fonts()
+    unload_fonts(font_regular, font_bold)
     ray.close_window()
 
 
